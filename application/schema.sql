d33c313ff4cadca5082161952e0b3c62907f45ea
Marc-André <mbarbeau@mapgears.com>
2013-01-04 18:43:52 +0000

    naturalwarth par defaut

--- application/schema.sql
+++ application/schema.sql
@@ -33,8 +33,23 @@ create table pois (
 
 
 INSERT INTO maps(map_id, map_name, map_desc, ws_id) VALUES("0","*default","Template par defaut","0");
+INSERT INTO maps(map_id, map_name, map_desc, ws_id) VALUES("1","*NaturalEarth","Template Natural Earth","0");
 
-INSERT INTO groups(group_name, group_index, map_id) VALUES("landuse", "1", "0");
-INSERT INTO groups(group_name, group_index, map_id) VALUES("land", "0", "0");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("cities", "6", "0");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("roads", "5", "0");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("admin", "4", "0");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("water", "3", "0");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("urban", "2", "0");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("land", "1", "0");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("ocean", "0", "0");
+
+INSERT INTO groups(group_name, group_index, map_id) VALUES("cities", "6", "1");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("roads", "5", "1");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("admin", "4", "1");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("water", "3", "1");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("urban", "2", "1");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("land", "1", "1");
+INSERT INTO groups(group_name, group_index, map_id) VALUES("ocean", "0", "1");
+
 
 INSERT INTO pois(poi_name, latitude, longitude, scalelvl, ws_id) VALUES("Quebec - Quebec","46","-71","10","0");
