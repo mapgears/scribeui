4636c2deb451c284ffdfc44c744cf025a9f87377
Samuel Lapointe <samuellapointe@gmail.com>
2015-07-23 13:21:15 -0400

    Fix load_plugin loop loading only one plugin

--- scribeui_pyramid/modules/plugins/__init__.py
+++ scribeui_pyramid/modules/plugins/__init__.py
@@ -24,9 +24,10 @@ def load_plugins():
     plugins = {}
     path = os.path.abspath(os.path.dirname(__file__))
     for filename in os.listdir(path):
-        if os.path.isdir(os.path.join(path, filename)) and os.path.isfile(os.path.join(path, filename, '__init__.py')):
+        tmp_path = path
+        if os.path.isdir(os.path.join(tmp_path, filename)) and os.path.isfile(os.path.join(tmp_path, filename, '__init__.py')):
             try:
-                f, pluginPath, descr = imp.find_module(filename, [path])
+                f, pluginPath, descr = imp.find_module(filename, [tmp_path])
                 pluginName = os.path.basename(pluginPath)
                 plugins[pluginName] = imp.load_module(filename, f, pluginName, descr)
             except ImportError:
